120a44c6a70d5f844233907802e8b227d2d4ba84
Henrique Nery <henriquenery07@gmail.com>
2024-06-16 00:55:23 -0300

    feat: add styles for contentItem

--- frontend/src/components/Contentitem.tsx
+++ frontend/src/components/Contentitem.tsx
@@ -7,9 +7,11 @@ type ContentItemProps = {
 
 const ContentItem: React.FC<ContentItemProps> = ({ id, title }) => {
   return (
-    <li className="my-2">
-      <Link legacyBehavior href={`/content/${id}`}>
-        <a className="text-blue-500 hover:underline">{title}</a>
+    <li className="my-4">
+      <Link href={`/content/${id}`}>
+      <div className='bg-[#7B7B82] bg-opacity-60 hover:bg-gray-800 transition-all duration-200 ease-in-out rounded-full shadow-lg p-4 w-[45rem] text-center'>
+        <a className='text-3xl font-bold font-gochi'>{title}</a>
+      </div>
       </Link>
     </li>
   );
