aed8120c12e581fbe6b1976963bb1c435afc3445
Henrique Nery <henriquenery07@gmail.com>
2024-06-16 13:34:46 -0300

    fix: modify font to h1 and h2

--- frontend/src/components/Contentitem.tsx
+++ frontend/src/components/Contentitem.tsx
@@ -7,7 +7,7 @@ const ContentItem: React.FC<Content> = ({ id, title }) => {
     <li className="my-4">
       <Link href={`/content/${id}`}>
       <div className='bg-[#7B7B82] bg-opacity-60 hover:bg-gray-800 transition-all duration-200 ease-in-out rounded-full shadow-lg p-4 w-[45rem] text-center'>
-        <a className='text-3xl font-bold font-gochi'>{title}</a>
+        <h2 className='text-3xl font-bold'>{title}</h2>
       </div>
       </Link>
     </li>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header className="text-center py-8 relative z-10">
-      <h1 className="text-4xl font-bold text-white">{title}</h1>
+      <h1 className="text-7xl font-bold text-white">{title}</h1>
     </header>
   );
 };
