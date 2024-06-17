1c972e9dc8709ca3e6385301c066fb88a91cbfb0
Henrique Nery <henriquenery07@gmail.com>
2024-06-16 22:58:54 -0300

    fix: modify heigth

--- frontend/src/components/ContentList.tsx
+++ frontend/src/components/ContentList.tsx
@@ -26,7 +26,7 @@ const ContentList: React.FC = () => {
   }
 
   return (
-    <div className='overflow-y-auto max-h-[360px] relative z-10'>
+    <div className='overflow-y-auto max-h-96 relative z-10'>
       <ul className='flex flex-col items-center justify-center'>
         {contents.map((content) => (
           <ContentItem key={content.id} id={content.id} title={content.title} />
