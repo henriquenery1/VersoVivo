b9f358fafcc8e2f3da87a732859c6b09b1f47bf5
Henrique Nery <henriquenery07@gmail.com>
2024-06-17 15:44:20 -0300

    fix: change opacity to 70

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ export default function SearchBar({ placeholder }: SearchBarProps) {
 
     return (
         <div className='flex items-center justify-center'>
-            <div className="bg-[#7B7B82] bg-opacity-60 w-[35rem] shadow-lg rounded-xl flex items-center justify-between px-4">
+            <div className="bg-[#7B7B82] bg-opacity-70 w-[35rem] shadow-lg rounded-xl flex items-center justify-between px-4">
                 <input
                     className="bg-transparent text-center py-[9px] outline-2 placeholder:text-gray-700 text-4xl font-normal flex-grow focus:outline-none"
                     placeholder={placeholder}
