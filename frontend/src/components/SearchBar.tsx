48eeae9076ac503ac2ae9946ead9c448c149c99c
Henrique Nery <henriquenery07@gmail.com>
2024-06-17 15:48:04 -0300

    fix: remove text center and add padding

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ export default function SearchBar({ placeholder }: SearchBarProps) {
         <div className='flex items-center justify-center'>
             <div className="bg-[#7B7B82] bg-opacity-70 w-[35rem] shadow-lg rounded-xl flex items-center justify-between px-4">
                 <input
-                    className="bg-transparent text-center py-[9px] outline-2 placeholder:text-gray-700 text-4xl font-normal flex-grow focus:outline-none"
+                    className="bg-transparent pl-4 py-[9px] outline-2 placeholder:text-gray-700 text-4xl font-normal flex-grow focus:outline-none"
                     placeholder={placeholder}
                     onChange={(e) => {
                         handleSearch(e.target.value);
