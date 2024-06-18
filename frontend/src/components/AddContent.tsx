37a9b6c2ad61e7255b9f03fe49c4330bd0fdf931
Henrique Nery <henriquenery07@gmail.com>
2024-06-17 23:05:41 -0300

    feat: add shadow

--- frontend/src/components/AddContent.tsx
+++ frontend/src/components/AddContent.tsx
@@ -1,10 +1,10 @@
 const AddContent: React.FC = () => {
     return (
         <div className="relative z-10">
-            <input type="text" placeholder="Título da obra" className="block mb-10 mx-auto bg-old-book-page rounded-lg h-20 w-2/4 p-4 text-3xl bg-opacity-80 placeholder:text-center placeholder:text-gray-700 text-content-pen" />
+            <input type="text" placeholder="Título da obra" className="block mb-10 mx-auto bg-old-book-page rounded-lg h-20 w-2/4 p-4 text-3xl bg-opacity-80 placeholder:text-center placeholder:text-gray-700 text-content-pen shadow-lg" />
 
-            <textarea className="mx-auto block bg-old-book-page rounded-lg h-[32rem] w-2/4 text-2xl bg-opacity-80 placeholder:text-center placeholder:text-gray-700 text-content-pen p-4 custom-scrollbar" 
-            placeholder="O que está pensando? ">
+            <textarea className="mx-auto block bg-old-book-page rounded-lg h-[32rem] w-2/4 text-2xl bg-opacity-80 placeholder:text-center placeholder:text-gray-700 text-content-pen p-4 custom-scrollbar shadow-lg" 
+            placeholder="O que está pensando?">
             </textarea>
         </div>
     );
