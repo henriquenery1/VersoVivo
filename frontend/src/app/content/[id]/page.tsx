326ec6506c69f511be9f84ee908e7b74e557b32c
Henrique Nery <henriquenery07@gmail.com>
2024-06-21 12:23:33 -0300

    refactor: content fetching logic into ShowContent component

--- frontend/src/app/content/[id]/page.tsx
+++ frontend/src/app/content/[id]/page.tsx
@@ -1,56 +1,17 @@
-"use client"
-
-import Background from "@/components/Background";
 import Header from "@/components/Header";
 import ShowContent from "@/components/ShowContent";
 import TextNavigation from "@/components/TextNavigation";
-import { Content, fetchContents } from "@/utils/fetchContent";
-import { useState, useEffect } from "react";
 
 interface Props {
   params: { id: string };
 }
 
 export default function ContentDetails({ params }: Props) {
-  const [contents, setContents] = useState<Content[] | null>(null);
-
-  useEffect(() => {
-    const loadContents = async () => {
-      const data = await fetchContents();
-      setContents(data);
-    };
-    loadContents();
-  }, []);
-
-  useEffect(() => {
-    const loadContents = async () => {
-      const data = await fetchContents();
-      const filteredContents = data.filter(
-        (content) => content.id === params.id
-      );
-      setContents(filteredContents);
-    };
-    loadContents();
-  }, [params.id]);
-
-  if (!contents || contents.length === 0) {
-    return (
-      <>
-        <Background />
-        <Header title='Nada por aqui...' />
-      </>
-    );
-  }
-
-  const content = contents[0];
-
   return (
     <main className="bg-image-bg h-screen">
-      <Header title={content.title} />
+      <ShowContent id={params.id} />
 
-      <ShowContent body={content.body} />
-
-    <TextNavigation paginate="/" title="Voltar" />
+      <TextNavigation paginate="/" title="Voltar" />
     </main>
   );
 }
--- frontend/src/components/ShowContent.tsx
+++ frontend/src/components/ShowContent.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import { Content, fetchContents } from "@/utils/fetchContent";
 
 interface Props {
-  body: string;
+  id: string;
 }
 
-const ShowContent: React.FC<Props> = ({ body }) => {
+const ShowContent: React.FC<Props> = ({ id }) => {
+  const [content, setContent] = useState<Content | null>(null);
+
+  useEffect(() => {
+    const loadContent = async () => {
+      const data = await fetchContents();
+      const content = data.find((item) => item.id === id) || null;
+      setContent(content);
+    };
+
+    loadContent();
+  }, [id]);
+
+  if (!content) {
+    return (
+      <main className="h-screen flex items-center justify-center">
+        <Header title="Carregando..." />
+      </main>
+    );
+  }
+
   return (
-    <div className="relative p-10 w-2/4 mx-auto shadow-lg bg-old-book-page bg-opacity-80 rounded-3xl overflow-auto max-h-[38rem] custom-scrollbar">
-      <p className="text-3xl text-content-pen leading-10 font-medium">
-        {body}
-      </p>
-    </div>
+    <>
+      <Header title={content.title} />
+
+      <div className="relative p-10 w-2/4 mx-auto shadow-lg bg-old-book-page bg-opacity-80 rounded-3xl overflow-auto max-h-[38rem] custom-scrollbar">
+
+        <article className="text-3xl text-content-pen leading-10 font-medium">
+          {content.body}
+        </article>
+
+      </div>
+    </>
   );
 };
 
